097fc44bf0c165f81ff069ecb4ee08db60b5d311
ZdravkoChorlev <30442544+ZdravkoChorlev@users.noreply.github.com>
2017-11-28 00:25:19 +0200

    Stack

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+
+    class Stack
+    {
+        static void Main(string[] args)
+        {
+            Encoding win = Encoding.GetEncoding("Windows-1251");
+            var cars = new StackExample();
+            cars.Push("Иван");
+
+            while (!cars.Empty)
+            {
+                string name = cars.Pop();
+                Console.WriteLine(name);
+            }
+        }
+    }
+
---
+++ QueueExample.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class StackExample
+{
+    public List<string> container = new List<string>();
+    public void Push(string value)
+    {
+        container.Add(value);
+    }
+    public string Pop()
+    {
+        string popped = container[container.Count - 1];
+        container.RemoveAt(container.Count - 1);
+        return popped;
+    }
+
+    public bool Empty
+    {
+        get { return container.Count == 0; }
+    }
+
+    public StackExample()
+    {
+        this.container = new List<string>();
+    }
+}
